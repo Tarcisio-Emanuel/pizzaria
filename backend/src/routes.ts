83e69065499803af120148f7cc991ef4c20e7c3b
Tarcisio <tarcisioemanoel10031@gmail.com>
2024-11-23 19:31:27 -0300

    BACKAND FINALIZADO

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,12 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import {ListCategoryController} from './controllers/category/ListCategoryController';
 import {CreateOrderController  } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AdditemController } from './controllers/order/AdditemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 
 import { CreateProductController } from "./controllers/product/CreateProductController";
@@ -44,6 +50,12 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // rotas order
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AdditemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
+router.get('/orders/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/orders/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 export { router }; 
