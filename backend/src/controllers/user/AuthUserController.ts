a18c65f15143893b19ee3db4e3e6db8f0e9a41ac
Tarcisio <tarcisioemanoel10031@gmail.com>
2024-11-26 23:30:24 -0300

    frontend

--- backend/src/controllers/user/AuthUserController.ts
+++ backend/src/controllers/user/AuthUserController.ts
@@ -2,9 +2,9 @@ import {Request, Response} from 'express';
 import { AuthUserService } from '../../services/user/AuthUserService';
 
 
-class AuthUserController{
+function AuthUserController(){
 
-    async handle(req:Request,  res: Response){
+     this.handle = async (req:Request,  res: Response) =>{
         const {email, password} = req.body;
         const  authUserService = new AuthUserService();
         const auth = await authUserService.execute({email, password});
